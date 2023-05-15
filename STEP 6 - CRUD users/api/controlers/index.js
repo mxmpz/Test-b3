cbd958eecd124ab92959f5c783ece9bb0a3e7db1
mxmpz <115069980+mxmpz@users.noreply.github.com>
2023-05-15 21:54:32 +0200

    Delete STEP 6 - CRUD users directory

--- STEP 6 - CRUD users/api/controlers/index.js
+++
@@ -1,9 +0,0 @@
-import statusCheck from './statusCheck.js'
-import bookCtrl from './bookCtrl.js'
-import userCtrl from './userCtrl.js'
-
-export default (repository) => ({
-  statusCheck,
-  bookCtrl: bookCtrl(repository.bookRepo),
-  userCtrl: userCtrl(repository.userRepo)
-})
--- STEP 6 - CRUD users/api/repository/index.js
+++
@@ -1,9 +0,0 @@
-import bookRepo from './bookRepo.js'
-import userRepo from './userRepo.js'
-import bookingRepo from './bookingRepo.js'
-
-export default (model) => ({
-  userRepo: userRepo(model.User),
-  bookRepo: bookRepo(model.Book),
-  bookingRepo: bookingRepo(model.Booking)
-})
